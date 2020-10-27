a148ef3471125c59a0fa452964d5aa29ac85d298
Cinthia <cinthia.j.23@gmail.com>
2020-10-27 15:14:52 +1100

    creating UI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Graph } from './graph';
 
+const textareaElement = document.querySelector('.textarea');
+const buttonElement = document.querySelector('.submit');
+
 const graph = new Graph(['AB5', 'BC4', 'CD8', 'DC8', 'DE6', 'AD5', 'CE2', 'EB3', 'AE7']);
 
 console.log(graph.findAmountOfRoutesByStops('A', 'C', 4));
