1ee8f64ff963a0f92359c9026bb21f4064e5f434
Cinthia <cinthia.j.23@gmail.com>
2020-10-27 13:48:46 +1100

    graph accepts inital data by constructor method

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 class Graph {
-  edges = {
-    A: { B: 5, D: 5, E: 7 },
-    B: { C: 4 },
-    C: { D: 8, E: 2 },
-    D: { C: 8, E: 6 },
-    E: { B: 3 },
-  };
+  edges = {};
+
+  constructor(routes) {
+    routes.forEach((route) => {
+      const [start, end, length] = route.split("");
+
+      if (!this.edges[start]) {
+        this.edges[start] = {};
+      }
+
+      this.edges[start][end] = Number(length);
+    });
+  }
 
   getDistance(path) {
     if (path.length < 2) {
@@ -107,7 +113,17 @@ class Graph {
   }
 }
 
-const graph = new Graph();
+const graph = new Graph([
+  "AB5",
+  "BC4",
+  "CD8",
+  "DC8",
+  "DE6",
+  "AD5",
+  "CE2",
+  "EB3",
+  "AE7",
+]);
 
 console.log(graph.findRoutesByStops("A", "C", 4));
 
