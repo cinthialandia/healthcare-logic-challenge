0c7465af01aa05316d01c1a6363fff0f25874183
Cinthia <cinthia.j.23@gmail.com>
2020-10-27 13:43:16 +1100

    Fix edges error and create function that finds distance by routes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ class Graph {
   edges = {
     A: { B: 5, D: 5, E: 7 },
     B: { C: 4 },
-    C: { D: 8, E: 8 },
+    C: { D: 8, E: 2 },
     D: { C: 8, E: 6 },
     E: { B: 3 },
   };
 
-  distance(path) {
+  getDistance(path) {
     if (path.length < 2) {
       throw new Error("NO SUCH ROUTE");
     }
@@ -36,7 +36,7 @@ class Graph {
     return result;
   }
 
-  findRoutes(start, end, limit = 3) {
+  getRoutes(start, end, limit = 3) {
     const paths = [];
 
     const visitNextNodes = (currentPath, currentNode, currentLimit) => {
@@ -59,15 +59,48 @@ class Graph {
     return paths;
   }
 
-  findExactRoutes(start, end, length) {
-    const paths = this.findRoutes(start, end, length);
+  findRoutesByDistance(start, end, maxDistance) {
+    if (!maxDistance) {
+      return;
+    }
+
+    const paths = [];
+
+    const visitNextNodes = (currentPath, currentNode, currentDistance) => {
+      if (currentDistance > maxDistance) {
+        return;
+      }
+
+      if (
+        currentNode[end] &&
+        currentDistance + currentNode[end] < maxDistance
+      ) {
+        paths.push([...currentPath, end]);
+      }
+
+      for (const next in currentNode) {
+        visitNextNodes(
+          [...currentPath, next],
+          this.edges[next],
+          currentDistance + currentNode[next]
+        );
+      }
+    };
+
+    visitNextNodes([start], this.edges[start], 0);
+
+    return paths;
+  }
+
+  findRoutesByStops(start, end, length) {
+    const paths = this.getRoutes(start, end, length);
 
     return paths.filter((path) => path.length === length + 1);
   }
 
-  findShortestRoute(start, end, limit) {
-    const routes = this.findRoutes(start, end, limit);
-    const routeDistances = routes.map((path) => this.distance(path));
+  findShortestRouteDistance(start, end, limit) {
+    const routes = this.getRoutes(start, end, limit);
+    const routeDistances = routes.map((path) => this.getDistance(path));
 
     const [shortestDistance] = routeDistances.sort((a, b) => a - b);
     return shortestDistance;
@@ -76,10 +109,12 @@ class Graph {
 
 const graph = new Graph();
 
-console.log(graph.findExactRoutes("A", "C", 4));
+console.log(graph.findRoutesByStops("A", "C", 4));
 
-console.log(graph.findRoutes("C", "C", 3));
+console.log(graph.getRoutes("C", "C", 3));
 
-console.log(graph.distance(["A", "B", "C"]));
+console.log(graph.getDistance(["A", "B", "C"]));
 
-console.log(graph.findShortestRoute("B", "B"));
+console.log(graph.findShortestRouteDistance("B", "B"));
+
+console.log(graph.findRoutesByDistance("C", "C", 30));
